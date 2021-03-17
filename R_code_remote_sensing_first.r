31478e2308815c01b0cea66ebc807769a5a54bbc
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-03-17 12:54:30 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -6,14 +6,14 @@ setwd("C:/lab/") # Windows
 # Codice per installare i pacchetti aggiuntivi raster (già installato)
 #install.packages("raster")
 
-#Funzione library per richiamare il pacchetto raster
+# Funzione library per richiamare il pacchetto raster
 library(raster)
 
-#Funzione brick per importare i dati
+# Funzione brick per importare i dati
 p224r63_2011<-brick("p224r63_2011_masked.grd")
 
-#Funzione per avere le info sul file
+# Funzione per avere le info sul file
 p224r63_2011
 
-#Funzione plot immagini per visualizzare le varie bande
+# Funzione plot immagini per visualizzare le varie bande
 plot(p224r63_2011)
