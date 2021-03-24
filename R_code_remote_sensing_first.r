91e107268b890120e0e29bfaf138028e0955a9d0
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-03-24 11:30:31 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -28,3 +28,29 @@ clp<-colorRampPalette(c("black","blue","pink","green","yellow"))(100)
 # Nuovo plot col cambio di colori
 plot(p224r63_2011, col=clp)
 
+
+# NUOVA IMMAGINE
+# Funzione library per richiamare il pacchetto raster
+library(raster)
+
+# Indicare la cartella da cui estrarre i dati
+setwd("C:/lab/") # Windows
+
+# Funzione brick per importare i dati
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+
+# Cambio di colori
+cls <- colorRampPalette(c("red","pink","orange","purple")) (200)
+plot(p224r63_2011, col=cls)
+
+# Funzione per avere le info sul file
+p224r63_2011
+
+# Spiegazione delle bande di LANDSAT
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: infrarosso vicino
+# B5: infrarosso medio
+# B6: infrarosso termico
+# B7: infrarosso medio
