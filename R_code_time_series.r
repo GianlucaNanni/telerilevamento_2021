4132adbf02c194682b44fbbc38b2bf1075800672
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-04-01 12:06:57 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -10,3 +10,10 @@ library(raster)
 
 # Indicare la cartella da cui estrarre i dati
 setwd("C:/lab/greenland") # Windows
+
+# Codice per installare pacchetto aggiuntivo
+install.packages("rasterVis")
+
+???
+# Funzione library per richiamare il pacchetto
+library(rasterVis)
