3c62321cd1076bfdf07b14cffa736f806743cf72
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-04-01 12:03:38 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -2,3 +2,11 @@
 # Groenlandia (Greenland) aumento della temperatura
 # Dati e codici di Emanuela Cosma
 
+# Codice per installare i pacchetti aggiuntivi raster (già installato)
+#install.packages("raster")
+
+# Funzione library per richiamare il pacchetto raster
+library(raster)
+
+# Indicare la cartella da cui estrarre i dati
+setwd("C:/lab/greenland") # Windows
