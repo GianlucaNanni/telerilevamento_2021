27f8959a6af5043e3d036e102fd4e11b721d4960
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-04-08 11:14:31 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -20,10 +20,21 @@ library(rasterVis)
 lst_2000 <- raster("lst_2000.tif")
 # Funzione plot: immagini per visualizzare le varie bande
 plot(lst_2000)
+# Nome immagine: visualizza valori min/max
+lst_2000
 
 # Funzione raster per importare singoli strati di dati
 lst_2005 <- raster("lst_2005.tif")
 # Funzione plot: immagini per visualizzare le varie bande
 plot(lst_2005)
-# Nome immagine: visualizza valori min/max
-lst_2005
+
+# Funzione raster per importare singoli strati di dati
+lst_2010 <- raster("lst_2010.tif")
+# Funzione plot: immagini per visualizzare le varie bande
+plot(lst_2010)
+
+# Funzione raster per importare singoli strati di dati
+lst_2015 <- raster("lst_2015.tif")
+# Funzione plot: immagini per visualizzare le varie bande
+plot(lst_2015)
+
