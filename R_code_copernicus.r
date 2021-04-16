989fba15acf5b37f9865bc61d8b2b1a3cb999cca
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-04-16 17:01:04 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -5,7 +5,7 @@
 library(raster)
 
 # Codice per installare pacchetto aggiuntivo: ncdf4
-install.packages("ncdf4")
+# install.packages("ncdf4") #già inserito
 
 # Funzione library per leggere file: .nc
 library(ncdf4)
@@ -16,3 +16,31 @@ setwd("C:/lab/") # Windows
 # Nome dataset: lswt (Lake Surface Water Temperature)
 # Funzione raster per importare un singolo layer (strato)
 lswt <- raster("c_gls_LSWT_202104010000_GLOBE_SLSTRAB_v1.1.0.nc")
+
+# Funzione per avere le info sul file
+lswt
+
+# Cambio di colori
+cl <- colorRampPalette(c('light blue','green','red','yellow'))(100)
+# Plot col cambio di colori
+plot(lswt, col=cl)
+
+# Riclassifico colori immagine
+lswt <- raster::reclassify(lswt, cbind(252:255, NA), right=TRUE)
+
+# resampling (ricampionamento)
+# Funzione aggregate per ridurre linearmente N° pixel immagine (=100), quindi si visualizza meglio
+lswtres <- aggregate(lswt, fact=100)
+# Nuovo plot col cambio di colori
+plot(lswtres, col=cl)
+
+#
+# Codice per installare pacchetto aggiuntivo: knitr
+install.packages("knitr")
+# Funzione library per richiamare il pacchetto: knitr
+library(knitr)
+
+# Codice per installare pacchetto aggiuntivo: RStoolbox
+install.packages("RStoolbox")
+# Funzione library per richiamare il pacchetto: RStoolbox
+library(RStoolbox)
