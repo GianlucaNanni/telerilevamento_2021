a242b77a12c323e8292a76d0f3034aefb903f85c
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-04-16 15:41:40 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -1,3 +1,18 @@
 # R_code_copernicus.r
 # Visualizzazione dati Copernicus
 
+# Funzione library per richiamare il pacchetto: raster
+library(raster)
+
+# Codice per installare pacchetto aggiuntivo: ncdf4
+install.packages("ncdf4")
+
+# Funzione library per leggere file: .nc
+library(ncdf4)
+
+# Indicare la cartella da cui estrarre i dati
+setwd("C:/lab/") # Windows
+
+# Nome dataset: lswt (Lake Surface Water Temperature)
+# Funzione raster per importare un singolo layer (strato)
+lswt <- raster("c_gls_LSWT_202104010000_GLOBE_SLSTRAB_v1.1.0.nc")
