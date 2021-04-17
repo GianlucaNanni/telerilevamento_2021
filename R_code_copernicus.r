ba3525de4cb536b6fcf6c71f1cf463ef23aed011
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-04-17 11:42:48 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -70,19 +70,3 @@ plot(vegp, col=cl)
 vegpres <- aggregate(vegp, fact=100)
 # Nuovo plot col cambio di colori
 plot(vegpres, col=cl)
-
-
-
-
-# USIAMO CODICE RINOMINATO (template) PER FARE FILE FINALE
-# Codice per installare pacchetto aggiuntivo: knitr
-# install.packages("knitr") #già installato
-# Funzione library per richiamare il pacchetto: knitr
-library(knitr)
-
-# Codice per installare pacchetto aggiuntivo: RStoolbox
-# install.packages("RStoolbox") #già installato
-# Funzione library per richiamare il pacchetto: RStoolbox
-library(RStoolbox)
-
-# Funzione stitch: creare automaticamente un report basato su uno script di R
