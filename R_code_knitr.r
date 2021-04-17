fc7a14a91d41086287bb498b2e9682fa07aaa9b7
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-04-17 11:44:43 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,1 +1,18 @@
 # R_code_knitr.r
+
+
+# USIAMO CODICE RINOMINATO (template) PER FARE FILE FINALE
+# Codice per installare pacchetto aggiuntivo: knitr
+# install.packages("knitr") #già installato
+# Funzione library per richiamare il pacchetto: knitr
+library(knitr)
+
+# Codice per installare pacchetto aggiuntivo: RStoolbox
+# install.packages("RStoolbox") #già installato
+# Funzione library per richiamare il pacchetto: RStoolbox
+library(RStoolbox)
+
+# Indicare la cartella da cui estrarre i dati
+setwd("C:/lab/") # Windows
+
+# Funzione stitch: creare automaticamente un report basato su uno script di R
