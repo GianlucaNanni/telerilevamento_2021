c6d97212bf9d519b779cce8af0490152c50e0cb6
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-04-21 11:24:11 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -1,2 +1,15 @@
 # R_code_classification.r
 
+# Funzione library per richiamare il pacchetto raster
+library(raster)
+
+# Indicare la cartella da cui estrarre i dati
+setwd("C:/lab/") # Windows
+
+# Funzione brick per importare i dati/immagine
+so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+
+# Funzione per avere le info sul 
+so
+
+# Visualizzare livelli RGB
