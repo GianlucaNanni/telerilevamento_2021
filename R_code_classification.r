4e721f77170cef7a5f455fe6ab8274e205ebb5ea
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-04-23 14:05:42 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -18,16 +18,20 @@ so
 # Funzione plotRGB: crea immagini multibanda
 plotRGB(so, 1, 2, 3, stretch="lin")
 
-# Funzione unsuperClass: opera la classificazione non supervisionata
+# Funzione unsuperClass: opera la classificazione non supervisionata (3 classi)
 soc <- unsuperClass(so, nClasses=3)
+# Funzione per avere le info sul file
+soc
 # Cambio di colori
 cl <- colorRampPalette(c('yellow','red','black'))(100)
 # Nuovo plot col cambio di colori
 plot(soc$map,col=cl) # Oppure plot(soc$map) se NON c'è la colorRampPalette prima
 
-# Funzione unsuperClass: opera la classificazione non supervisionata
+# Funzione unsuperClass: opera la classificazione non supervisionata (20 classi)
 # A 20 classi
 sotw <- unsuperClass(so, nClasses=20)
+# Funzione per avere le info sul file
+sotw
 # Cambio di colori
 cls <- colorRampPalette(c('yellow','red','black'))(100)
 # Nuovo plot col cambio di colori
@@ -40,6 +44,8 @@ sun <- brick("sun.png")
 
 # Funzione unsuperClass: opera la classificazione non supervisionata
 sunc <- unsuperClass(sun, nClasses=3)
+# Funzione per avere le info sul file
+sunc
 # Plot:
 plot(sunc$map)
 
@@ -67,3 +73,18 @@ plotRGB(gc, r=1, g=2, b=3, stretch="lin")
 # Funzione plotRGB: crea immagini multibanda ad alto contrasto
 plotRGB(gc, r=1, g=2, b=3, stretch="hist")
 
+# Funzione unsuperClass: opera la classificazione non supervisionata (2 classi)
+gcc2 <- unsuperClass(gc, nClasses=2)
+# Funzione per avere le info sul file
+gcc2
+# Plot:
+plot(gcc2$map)
+
+# Funzione unsuperClass: opera la classificazione non supervisionata (4 classi)
+gcc4 <- unsuperClass(gc, nClasses=4)
+# Funzione per avere le info sul file
+gcc4
+# Cambio di colori
+clrpp <- colorRampPalette(c('red','purple','orange','pink'))(100)
+# Nuovo plot col cambio di colori
+plot(gcc4$map,col=clrpp) 
