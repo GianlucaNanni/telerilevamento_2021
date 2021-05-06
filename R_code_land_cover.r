928221bb292d253230207573a1e22af34953692b
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-05-06 19:01:11 +0200

    Create R_code_land_cover.r

---
+++ R_code_land_cover.r
@@ -0,0 +1,11 @@
+# R_code_land_cover.r
+
+# Indicare la cartella da cui estrarre i dati
+setwd("C:/lab/") # Windows
+
+# Funzione library per richiamare il pacchetto: raster
+library(raster) #oppure: require(raster)
+
+# Funzione library per richiamare il pacchetto: RStoolbox
+library(RStoolbox)
+
