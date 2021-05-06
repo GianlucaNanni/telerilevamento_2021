85a771d4ce9268a24e2a32dced5556dd3e908108
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-05-06 19:13:49 +0200

    Update R_code_land_cover.r

--- R_code_land_cover.r
+++ R_code_land_cover.r
@@ -9,3 +9,18 @@ library(raster) #oppure: require(raster)
 # Funzione library per richiamare il pacchetto: RStoolbox
 library(RStoolbox)
 
+# Codice per installare pacchetto aggiuntivo: ggplot2
+# install.packages("ggplot2") #già installato
+# Funzione library per richiamare il pacchetto: ggplot2
+library(ggplot2)
+
+# Funzione brick per importare i dati/immagine
+defor1 <- brick("defor1.jpg")
+# Funzione plotRGB: crea immagini multibanda
+plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
+
+# Funzione brick per importare i dati/immagine
+defor2 <- brick("defor2.jpg")
+# Funzione plotRGB: crea immagini multibanda
+plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
+
