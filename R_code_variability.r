902f57023d9f4c8aefe32b61a18f0844a0935698
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-05-21 15:07:31 +0200

    Create R_code_variability.r

---
+++ R_code_variability.r
@@ -0,0 +1,17 @@
+# R_code_variability.r
+
+# Indicare la cartella da cui estrarre i dati
+setwd("C:/lab/") # Windows
+
+# Funzione library per richiamare il pacchetto: raster
+library(raster) #oppure: require(raster)
+
+# Funzione library per richiamare il pacchetto: RStoolbox
+library(RStoolbox)
+
+# Funzione library per richiamare il pacchetto: ggplot2
+library(ggplot2)
+
+# Funzione library per richiamare il pacchetto: gridExtra
+library(gridExtra)
+
