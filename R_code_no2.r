9056a3d2a5d7177d7bfacb64fbc220349605ae1a
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-07-15 11:58:42 +0200

    Update R_code_no2.r

--- R_code_no2.r
+++ R_code_no2.r
@@ -11,3 +11,34 @@ library(raster) #oppure: require(raster)
 # Funzione raster per importare i singoli strati di dati: importa una singola banda (la 1°)
 EN01 <- raster("EN_0001.png")
  
+# 3. Plot della prima immagine importata con ColorRampPalette
+# Cambio di colori N.B: colorRampPalette si può usare solo su immagine singole, non su RGB
+cls <- colorRampPalette(c("red","pink","orange","yellow")) (200)
+plot(EN01, col=cls)
+
+# 4. Importare l'ultima immagine (la 13°) e plottare con ColorRampPalette
+# Funzione raster per importare i singoli strati di dati: importa una singola banda (la 13°)
+EN13 <- raster("EN_0013.png")
+
+# Cambio di colori (ColorRampPalette)
+cls <- colorRampPalette(c("red","pink","orange","yellow")) (200)
+plot(EN13, col=cls)
+
+# 5. Vedere la differenza tra le 2 immagini e fare il plot
+# Se faccio la differenza tra mappa di marzo (valore basso) e gennaio (valore alto) la differenza sarà negativa (viceversa sarà positivo)
+ENdif <- EN13 - EN01
+# Se faccio la differenza tra mappa di gennaio (valore alto) e marzo (valore basso) la differenza sarà positiva (viceversa sarà negativa)
+ENdif <- EN01 - EN13
+# Plot
+plot(ENdif, col=cls)
+
+# 6. Plot di tutte le immagini insieme
+# Funzione par: imposta più parametri grafici nella stessa immagine
+par(mfrow=c(3,1))
+plot(EN01, col=cls, main="NO2 in January")
+plot(EN13, col=cls, main="NO2 in March")
+plot(ENdif, col=cls, main="Difference (January - March)")
+
+
+
+
