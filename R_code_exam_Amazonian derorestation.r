39475b517979e0b8acd559b29934672e7b07cbe6
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-07-29 08:50:45 +0200

    Update R_code_exam_Amazonian derorestation.r

--- R_code_exam_Amazonian derorestation.r
+++ R_code_exam_Amazonian derorestation.r
@@ -5,3 +5,10 @@
 setwd("C:/lab/amazonian") # Windows
 
 # Funzioni: library, necessarie per richiamare i pacchetti per la realizzazione del progetto
+require(raster)
+library(rasterdiv)
+library(RStoolbox) # for vegetation indices calculation
+
+# Importare i file tutti insieme (invece che singolarmente)
+# Funzione list.files: crea lista di file per la funzione lapply 
+rlist <- list.files(pattern="amazon_deforestation") # pattern=scritta in comune nei file
