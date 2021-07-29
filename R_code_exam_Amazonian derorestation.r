f09756a417ddcb5902330d514ccacd6f88850257
GianlucaNanni <80623257+GianlucaNanni@users.noreply.github.com>
2021-07-29 08:40:51 +0200

    Create R_code_exam_Amazonian derorestation.r

---
+++ R_code_exam_Amazonian derorestation.r
@@ -0,0 +1,7 @@
+# R_code_exam_Amazonian derorestation.r
+# Deforestazione Amazzonica avvenuta tra il 2001 e 2011
+
+# Indicare la cartella da cui estrarre i dati
+setwd("C:/lab/amazonian") # Windows
+
+# Funzioni: library, necessarie per richiamare i pacchetti per la realizzazione del progetto
